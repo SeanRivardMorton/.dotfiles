62f1940cd3e37da9e208d14f3f5ca0c25422358a
Sean <sean@sean-work.local>
2025-02-08 11:36:08 +0000

    readd nvim

---
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -0,0 +1,39 @@
+return {
+  'folke/snacks.nvim',
+  ---@type snacks.Config
+  opts = {
+    lazygit = {
+      -- your lazygit configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    win = {},
+    terminal = {},
+    gitbrowse = {},
+    notifier = {},
+  },
+  keys = {
+    {
+      '<leader>lg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
+    {
+      '<leader>`',
+      function()
+        Snacks.terminal.toggle('here', opts)
+        -- Snacks.terminal.open(cmd, opts)
+        -- Snacks.win()
+      end,
+    },
+    {
+      '<leader>gb',
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = 'Gitbrowse',
+    },
+  },
+}
